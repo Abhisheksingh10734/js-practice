c187e01c15a2742f085b0a10dd6493687a3872a6
Abhishek Singh <abhisingh10734@gmail.com>
2026-07-21 01:55:00 +0530

    string problem 3 add

--- 03-strings/01_Basics/03-find-string-length.js
+++ 03-strings/01_Basics/03-find-string-length.js
@@ -1,15 +1,15 @@
 // Problem :- Find the length of a string without using .length directly inside the loop.
 
-const str = "Javascript";
+let str = 'Javascript';
 
 let strLen = 0;
 
-// Using while loop
-while(str[strLen] !== undefined) {
-    strLen++;
+while (str[strLen] !== undefined) {
+    strLen++
 }
 
-// using for of loop
-for(const len of str) {
-    strLen++;
+for (let ch of str) {
+    strLen++
 }
+
+console.log(strLen);
