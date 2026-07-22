98a5c0d4285d66ee5a855b01465c03939b9247f6
Abhishek Singh <abhisingh10734@gmail.com>
2026-07-22 11:19:39 +0530

    extract first five char add

---
+++ 03-strings/04_Substring/21-extract-first-five-characters.js
@@ -0,0 +1,11 @@
+// Problem :- Extract the first 5 characters.
+
+const str = 'Javascript';
+
+console.log(str.slice(0,5));
+
+// usign loop
+
+for(let i = 0; i < 5; i++) {
+        console.log(str[i]);
+}
